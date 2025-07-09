703553186e0ad3d65d975f7677c22dadd12557a3
sreenath <64717275+sreenatgh4757@users.noreply.github.com>
2025-07-09 21:45:36 +0100

    Updated ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -78,7 +78,7 @@ const ContactForm: React.FC = () => {
                 <div>
                   <h4 className="font-semibold text-white mb-1">Office Location</h4>
                   <p className="text-gray-300">
-                    London, United Kingdom
+                    Bournemouth, United Kingdom
                   </p>
                 </div>
               </div>
