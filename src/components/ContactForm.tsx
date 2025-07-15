d276bc09dc99e82422e72a2420511d80fe7bed37
sreenath <64717275+sreenatgh4757@users.noreply.github.com>
2025-07-15 22:38:44 +0100

    Fix ContactForm component export error

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactForm: React.FC = () => {
+  return (
+    <div>
+      <h2>Contact Form</h2>
+      <p>Contact form component placeholder</p>
+    </div>
+  );
+};
+
+export default ContactForm;
