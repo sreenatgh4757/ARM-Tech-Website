d01791e5184623206e95118bc2c3bd383c99f5b8
sreenath <64717275+sreenatgh4757@users.noreply.github.com>
2025-07-15 22:40:35 +0100

    Add default export to ContactForm component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactForm: React.FC = () => {
+  return (
+    <div>
+      {/* Contact form component */}
+    </div>
+  );
+};
+
+export default ContactForm;
